8f12acf16fe2f62328eb0bf38e1b9d8b4f8d0781
Pax-Amandi <9334590+Pax-Amandi@users.noreply.github.com>
2018-02-14 12:41:49 +0300

    Update main.py

--- 11/main.py
+++ 11/main.py
@@ -1,9 +1,20 @@
+#!/usr/bin/env python
+
+__author__ = ""
+__copyright__ = ""
+__email__ = ""
+
+
+
 def height(a,b,s):
     """
+    
     docstring располагается здесь 
+    
     """
-    pass  # ваш код располагается здесь
-    # return
+    
+    # ваш код располагается ниже этой строки 
+    pass  # pass можно удалить
 
 
 if __name__ == '__main__':
